034e2247a98922daaf5c03c0120488b328bb7b31
EC2 Default User <ec2-user@ip-10-10-12-40.eu-west-1.compute.internal>
2020-11-02 11:30:54 +0000

    update terraformtesting

--- terraform/mytest/instances.tf
+++ terraform/mytest/instances.tf
@@ -2,9 +2,9 @@
 # MAA SERVER
 #-----------------------------------------------------
 resource "aws_instance" "example" {
-  ami			  = "ami-0bb3fad3c0286ebd5"
+  ami			  = "ami-0823c236601fef765"
   instance_type           = "t2.medium"
-  vpc_security_group_ids  = ["sg-1f879a65", "sg-48849932"]
+  vpc_security_group_ids  = [aws_security_group.open8080.id]
   subnet_id               = "subnet-4175af27"
   iam_instance_profile        = "InspectorRole"
   associate_public_ip_address = "true"
@@ -17,9 +17,7 @@ resource "aws_instance" "example" {
 #!/bin/bash
 echo "Hello, World!" > index.html
 nohup busybox httpd -f -p 8080 &
-EOF  
-
-
+EOF
 
   root_block_device {
     volume_type = "gp2"
@@ -28,6 +26,7 @@ EOF
 } 
 
 resource "aws_security_group" "open8080" {
+  vpc_id      = "vpc-75128013"
   ingress {
     from_port   = 8080
     to_port     = 8080
